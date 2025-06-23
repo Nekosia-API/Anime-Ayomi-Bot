ada8b071d3898ada8b9aa666bc18066fff75c220
Sefinek <contact@sefinek.net>
2025-06-23 13:12:13 +0200

    Add summer.js

---
+++ commands/anime/summer.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const fetchImage = require('../../scripts/fetchImage.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('summer')
+		.setDescription('Get anime summer girls with swimsuits, beach hats, and sea breeze vibes.')
+		.addIntegerOption(option =>
+			option.setName('count')
+				.setDescription('How many adorable images would you like? (1 to 5)')
+				.setMinValue(1)
+				.setMaxValue(5))
+		.addBooleanOption(option =>
+			option.setName('compressed')
+				.setDescription('Display compressed (lighter and faster-loading) images. Default: false'))
+		.setDefaultMemberPermissions(PermissionFlagsBits.AttachFiles),
+
+	execute: (_, inter) => fetchImage(inter, 'summer'),
+};
