a09c3756ce51c76b154aab90c76cf78092fe24f4
Sefinek <contact@sefinek.net>
2025-09-13 15:03:06 +0200

    Update spamGuildEmbed.js

--- scripts/spamGuildEmbed.js
+++ scripts/spamGuildEmbed.js
@@ -16,17 +16,18 @@ module.exports = async (client, guild, log = false) => {
 	if (!channel) return null;
 
 	try {
-		await channel.send({
-			embeds: [new EmbedBuilder()
-				.setColor('#ff4e4e')
-				.setAuthor({ name: 'I\'m leaving this server, sayonara! 👋', iconURL: guild.iconURL(), url: 'https://nekosia.cat' })
-				.setDescription(
-					'Pfft. This server stinks. I have no intention of taking part in artificially inflating stats. I assume it was created only to promote another server using Disboard.\n\n' +
-                    'If you think my judgment is wrong (which I doubt), the only way is to contact my developer via the [support server](https://nekosia.cat/discord) or email `support@nekosia.cat`. Bye!'
-				)
-				.setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL() })
-				.setTimestamp()],
-		});
+		const embed = new EmbedBuilder()
+			.setColor('#ff4e4e')
+			.setAuthor({ name: 'I\'m leaving this server, sayonara! 👋', iconURL: guild.iconURL(), url: 'https://nekosia.cat' })
+			.setDescription(
+				'Pfft. This server stinks. I have no intention of taking part in artificially inflating stats. I assume it was created only to promote another server using Disboard.\n\n' +
+                'If you think my judgment is wrong (which I doubt), the only way is to contact my developer via the [support server](https://nekosia.cat/discord) or email `support@nekosia.cat`. Bye!'
+			)
+			.setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL() })
+			.setTimestamp();
+
+		await channel.send({ embeds: [embed] });
+		await client.users.fetch(guild.ownerId).send({ embeds: [embed] }).catch(err => console.log('Client » Message to owner was not delivered.', err.message));
 
 		if (log) console.log(`Sent leave message in '${guild.name}' (${guild.id}) -> #${channel.name} (${channel.id})`);
 
